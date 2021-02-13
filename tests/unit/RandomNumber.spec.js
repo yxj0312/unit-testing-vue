472f7800e49f37054797bebab79f2e518c0547c6
yxj0312 <yanxiajun@hotmail.com>
2021-02-13 23:00:42 +0100

    make test3 pass

--- tests/unit/RandomNumber.spec.js
+++ tests/unit/RandomNumber.spec.js
@@ -15,7 +15,18 @@ describe('RandomNumber', () => {
     expect(randomNumber).toBeGreaterThanOrEqual(1)
     expect(randomNumber).toBeLessThanOrEqual(10)
   })
-  test('If we change the min and max props to 200 and 300 and click the button, randomNumber should be between 200 and 300', () => {
-    expect(true).toBe(false)
+  test('If we change the min and max props to 200 and 300 and click the button, randomNumber should be between 200 and 300', async () => {
+    const wrapper = mount(RandomNumber, {
+      propsData: {
+        min: 200,
+        max: 300
+      }
+    })
+    wrapper.find('button').trigger('click')
+
+    await wrapper.vm.$nextTick()
+    const randomNumber = parseInt(wrapper.find('span').element.textContent)
+    expect(randomNumber).toBeGreaterThanOrEqual(200)
+    expect(randomNumber).toBeLessThanOrEqual(300)
   })
 })
