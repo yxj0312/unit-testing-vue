2b3c985e465dced987a7038cfa3d569e317a1bfd
yxj0312 <yanxiajun@hotmail.com>
2021-02-13 22:53:00 +0100

    get test 2 pass

--- tests/unit/RandomNumber.spec.js
+++ tests/unit/RandomNumber.spec.js
@@ -6,8 +6,14 @@ describe('RandomNumber', () => {
     const wrapper = mount(RandomNumber)
     expect(wrapper.html()).toContain('<span>0</span>')
   })
-  test('If we click the generate button, randomNumber should be between 1 and 10', () => {
-    expect(true).toBe(false)
+  test('If we click the generate button, randomNumber should be between 1 and 10', async () => {
+    const wrapper = mount(RandomNumber)
+    wrapper.find('button').trigger('click')
+
+    await wrapper.vm.$nextTick()
+    const randomNumber = parseInt(wrapper.find('span').element.textContent)
+    expect(randomNumber).toBeGreaterThanOrEqual(1)
+    expect(randomNumber).toBeLessThanOrEqual(10)
   })
   test('If we change the min and max props to 200 and 300 and click the button, randomNumber should be between 200 and 300', () => {
     expect(true).toBe(false)
