6af0f75b0da22e56c36bf0e133651f79b0fb8699
yxj0312 <yanxiajun@hotmail.com>
2021-02-13 22:35:33 +0100

    init test

---
+++ tests/unit/RandomNumber.spec.js
@@ -0,0 +1,14 @@
+import RandomNumber from '@/components/RandomNumber'
+import { mount } from '@vue/test-utils'
+
+describe('RandomNumber', () => {
+  test('By default, the randomNumber data value should be 0', () => {
+    expect(true).toBe(false)
+  })
+  test('If we click the generate button, randomNumber should be between 1 and 10', () => {
+    expect(true).toBe(false)
+  })
+  test('If we change the min and max props to 200 and 300 and click the button, randomNumber should be between 200 and 300', () => {
+    expect(true).toBe(false)
+  })
+})
