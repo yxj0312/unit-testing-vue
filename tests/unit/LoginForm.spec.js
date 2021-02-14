5223ac0b3bdfcbef3f98d52dc4624fc049e8b7c3
yxj0312 <yanxiajun@hotmail.com>
2021-02-14 21:58:58 +0100

    Add  Scaffolding of test

---
+++ tests/unit/LoginForm.spec.js
@@ -0,0 +1,13 @@
+import LoginForm from '@/components/LoginForm.vue'
+import { mount } from '@vue/test-utils'
+
+describe('LoginForm', () => {
+  it('emits an event with a user data payload', () => {
+    const wrapper = mount(LoginForm)
+    // 1. Find text input
+    // 2. Set value for text input
+    // 3. Simulate form submission
+    // 4. Assert event has been emitted
+    // 5. Assert payload is correct
+  })
+})
