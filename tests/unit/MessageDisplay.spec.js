dc7c2440b8d4a3cfb0d9aeda383bd4115faf0527
yxj0312 <yanxiajun@hotmail.com>
2021-02-16 22:55:15 +0100

    first test

--- tests/unit/MessageDisplay.spec.js
+++ tests/unit/MessageDisplay.spec.js
@@ -14,14 +14,19 @@ describe('MessageDisplay', () => {
     // wait for promise to resolve
     await flushPromises()
     // check that call happened once
+    expect(getMessage).toHaveBeenCalledTimes(1)
     // check that component displays message
+    const message = wrapper.find('[data-testid="message"]').element.textContent
+    expect(message).toEqual(mockMessage)
   })
 
   it('Displays an error when getMessage call fails', async () => {
     // mock the failed API call
     const wrapper = mount(MessageDisplay)
     // wait for promise to resolve
-    // check that call happened once
+    await flushPromises()
+    // check that call happened
+
     // check that component displays error
   })
 })
